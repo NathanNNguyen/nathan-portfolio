5fc57890c1e04caac69673146dc48b05aed1d904
Nhan <natenguyen51294@gmail.com>
2020-04-27 10:29:47 -0700

    structure components, added route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Home, About, Project, Contact } from './components';
 
 function App() {
   return (
     <div className="app">
-      <h1>App</h1>
+      <Router>
+        <Switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+
+          <Route exact path='/about'>
+            <About />
+          </Route>
+
+          <Route exact path='/project'>
+            <Project />
+          </Route>
+
+          <Route exact path='/contact'>
+            <Contact />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Project/Project.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Project() {
+  return (
+    <div className="Project">
+      <h1>Project</h1>
+    </div>
+  );
+}
+
+export default Project;
